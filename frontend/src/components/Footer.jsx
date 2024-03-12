b7ca46d556552780558a81e27fa34237b7e943f6
Filmona <zimilifila3676@gmail.com>
2024-03-13 00:43:40 +0300

    Logo Changer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,7 +8,7 @@ const Footer = () => {
       <Container>
         <Row>
           <Col className='text-center py-3'>
-            <p>ProShop &copy; {currentYear}</p>
+            <p>Designed & Developed By Filmon, Natinael & Yosef &copy; {currentYear}</p>
           </Col>
         </Row>
       </Container>
--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ import { useNavigate } from 'react-router-dom';
 import { useLogoutMutation } from '../slices/usersApiSlice';
 import { logout } from '../slices/authSlice';
 import SearchBox from './SearchBox';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo.jpeg';
 import { resetCart } from '../slices/cartSlice';
 
 const Header = () => {
@@ -37,8 +37,8 @@ const Header = () => {
         <Container>
           <LinkContainer to='/'>
             <Navbar.Brand>
-              <img src={logo} alt='ProShop' />
-              ProShop
+              <img src={logo} style={{width:"30px",marginRight:"5px"}} alt='ProShop' />
+              ET Gebeya
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
