96fb49533da04c3f55edb46b796758b40c9ab642
Filmona <zimilifila3676@gmail.com>
2024-06-08 09:06:18 +0300

    add new styling

--- frontend/src/components/ProductCarousel.jsx
+++ frontend/src/components/ProductCarousel.jsx
@@ -13,7 +13,7 @@ const ProductCarousel = () => {
       {products.map((product) => (
         <Carousel.Item key={product._id}>
           <Link to={`/product/${product._id}`}>
-            <Image  src={product.image} alt={product.name} fluid />
+            <Image  src={product.image} style={{height:400}} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h4 className='text-white text-right'>
                 {product.name} (${product.price})
